de8f617281289a6aa16048566c8873c607214f3d
Lars Wassermann <lars.wassermann@googlemail.com>
2018-09-23 14:09:02 +0200

    Visualize progress during selection

--- src/ChooseTalent.js
+++ src/ChooseTalent.js
@@ -9,6 +9,17 @@ import { attributes } from './constants'
 import Header from './Header'
 import Main from './Main'
 
+function Selected({ attributes }) {
+  return (
+    <div>
+      <Typography paragraph>
+        Test ({attributes.join('·')}
+        ·?)
+      </Typography>
+    </div>
+  )
+}
+
 export default function ChooseTalent({ match }) {
   const prefix = `${match.url}/`
 
@@ -17,6 +28,9 @@ export default function ChooseTalent({ match }) {
       <Header title="Talent challenge" />
 
       <Main>
+        {match.params.attr && (
+          <Selected attributes={Object.values(match.params)} />
+        )}
         <Typography>Select the next attribute</Typography>
         <List>
           {attributes.map(({ label, abbreviation }) => (
