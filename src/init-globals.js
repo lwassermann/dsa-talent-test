cd63f32af2664a8631ec4737abda5f53e6a5ce76
Lars Wassermann <lars.wassermann@googlemail.com>
2018-09-23 14:42:43 +0200

    Load global char from localstorage

--- src/init-globals.js
+++ src/init-globals.js
@@ -1,12 +1,18 @@
+import { attributes } from './constants'
+
 global.character = {
   attributes: {
-    bravery: 10,
-    intelligence: 10,
-    intuition: 10,
-    charisma: 10,
-    dexterity: 10,
-    agility: 10,
-    konstitution: 10,
-    strength: 10
+    // bravery: 10,
+    // intelligence: 10,
+    // intuition: 10,
+    // charisma: 10,
+    // dexterity: 10,
+    // agility: 10,
+    // konstitution: 10,
+    // strength: 10
   }
 }
+
+attributes.forEach(({ name }) => {
+  global.character.attributes[name] = localStorage.getItem(name) || 10
+})
